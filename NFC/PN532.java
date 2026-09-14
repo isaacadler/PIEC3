922742c2a0eacb2acdfa2a54760c00bf57c6aabd
isaac <isaac.adler@ufrpe.br>
2026-09-14 16:56:18 -0300

    Atributos, construtor criados e metodo de ler UID a implementar #4

--- NFC/PN532.java
+++ NFC/PN532.java
@@ -4,5 +4,28 @@ import com.pi4j.io.i2c.I2C;
 import com.pi4j.io.i2c.I2CConfig;
 
 public class PN532 {
+    private static final int ENDERECO_I2C = 0x24;
 
+    private Context pi4j;
+    private I2C i2c;
+
+    public void iniciar() {
+
+        pi4j = Pi4J.newAutoContext();
+
+        I2CConfig config = I2C.newConfigBuilder(pi4j)
+                .id("PN532")
+                .bus(1)
+                .device(ENDERECO_I2C)
+                .build();
+
+        i2c = pi4j.create(config);
+
+        System.out.println("I2C conectado ao PN532.");
+    }
+
+    public String lerUID() {
+
+        return null;
+    }
 }
