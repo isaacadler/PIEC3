faef873e9fb7d059dc8e9874744f007d61dbbb1c
isaac <isaac.adler@ufrpe.br>
2026-09-14 16:59:22 -0300

    Instanciando PN532 para ler NFC #4

--- NFC/Main.java
+++ NFC/Main.java
@@ -1,5 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
+        PN532 pn532 = new PN532();
 
+        pn532.iniciar();
+
+        System.out.println("Leitor iniciado");
+        System.out.println("Aproxime um NFC");
+
+        while (true) {
+            String uid = pn532.lerUID();
+
+            if (uid != null) {
+                System.out.println("UID encontrado");
+                System.out.println("UID: " + uid);
+                System.out.println();
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
     }
